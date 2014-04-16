5b8c4d7c5edefca0f4cc00b3b3a62d6d14b195b8
ramnes <contact@ramnes.eu>
2014-04-16 17:49:32 +0100

    Let start our Twitter clone :-)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trotter::Application.config.secret_token = '8faf0d5350d10bc0d4c7d6e0e98ce8447422defdd10fe0ad558ab3983a30a23727a1db1684063cdeac814894f9061a5863ba35aeea681fd462f716eeee3a8ac9'
